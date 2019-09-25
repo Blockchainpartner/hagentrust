bd524b6ac213055cae1839bfea9533ea6bee1ff4
tchataigner <tom.chataigner@yahoo.fr>
2019-09-25 18:01:53 +0200

    :construction: Working removed claim & added topics in entries

--- graph/src/mapping.ts
+++ graph/src/mapping.ts
@@ -9,24 +9,24 @@ import { ExampleEntity } from "../generated/schema"
 export function handleClaimSet(event: ClaimSet): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = ExampleEntity.load(event.transaction.from.toHex());
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    entity = new ExampleEntity(event.transaction.from.toHex());
 
     // Entity fields can be set using simple assignments
     entity.count = BigInt.fromI32(0)
   }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  entity.count = entity.count + BigInt.fromI32(1);
 
   // Entity fields can be set based on event parameters
-  entity.issuer = event.params.issuer
-  entity.recipient = event.params.recipient
-
+  entity.issuer = event.params.issuer;
+  entity.recipient = event.params.recipient;
+  entity.topic = event.params.topic;
   // Entities can be written to the store with `.save()`
   entity.save()
 
@@ -59,4 +59,12 @@ export function handleClaimSet(event: ClaimSet): void {
   // - contract.getTopics(...)
 }
 
-export function handleClaimRemoved(event: ClaimRemoved): void {}
+export function handleClaimRemoved(event: ClaimRemoved): void {
+  let entity = ExampleEntity.load(event.transaction.from.toHex());
+
+  // BigInt and BigDecimal math are supported
+  entity.count = entity.count - BigInt.fromI32(1);
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+}
